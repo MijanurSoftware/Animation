c8d74b7768a1bd848ea362d194ac53b37539d768
MijanurSoftware <mijanurrahmandinajpur5@gmail.com>
2023-09-11 21:02:44 +0600

    Another Animations Xml

---
+++ app/src/main/java/com/mrsoft/animationd/MainActivity.java
@@ -0,0 +1,69 @@
+package com.mrsoft.animationd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        MyAdapter myAdapter = new MyAdapter();
+        listView.setAdapter(myAdapter);
+
+
+
+    }
+
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 20;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View myView = layoutInflater.inflate(R.layout.layout, viewGroup, false);
+
+            TextView item = myView.findViewById(R.id.item);
+
+
+            Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale);
+            myView.startAnimation(animation);
+
+
+
+            return myView;
+        }
+    }
+
+}
